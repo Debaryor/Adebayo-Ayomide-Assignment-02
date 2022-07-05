e86191318eb3d486c33b28df97d9166644e587a5
Adebayo Ayomide <98376096+Debaryor@users.noreply.github.com>
2022-07-05 10:42:20 +0000

    Attempt 10

--- src/assignment.js
+++ src/assignment.js
@@ -13,12 +13,11 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers.*/
 
-function sumOfNumbers(){ 
+function sumOfNumbers(){
   
-let arrayOfNumbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, -50, 34, -86, 90, 81, 20,-56,-91, 4]  
-  
+let arrayOfNumbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20,-56,-91,-50]
   let sum = 0;
-  for(let i=0; i < arrayOfNumbers.length; i++){ 
+  for(let i=0; i < arrayOfNumbers.length; i++){
      sum += arrayOfNumbers[i]
   };
      return sum;
@@ -36,11 +35,12 @@ assignment.sumOfNumbers = sumOfNumbers;
  */
 
 function countEvenNumbers() {
-  
-  let arrayOfNumbers = [17, 0, 67, 41, 49, 21, 7, 86, 76, 64, 54, 14, 93, 84, 14, 77, 35, 1, 29, 2]
+
+  let arrayOfNumbers = [17, 0, 67, 41, 49, 21, 1, 86, 76, 64, 54, 14, 93, 84, 14, 77, 35, 7, 29, 2]
   
   let newArray = []
   for(let i = 0; i < arrayOfNumbers.length; i++){
+
     let even = arrayOfNumbers[i];
     if (even % 2 === 0){
       newArray.push(even)
